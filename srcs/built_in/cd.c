681ad7c8f3d02bf2d679d59ac8aed02782e80649
Oswalde PASSIN <opassin@stdudent.42.fr>
2022-10-25 00:05:38 +0200

    builtin echo fix

--- srcs/built_in/cd.c
+++ srcs/built_in/cd.c
@@ -6,7 +6,7 @@
 /*   By: opassin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 13:31:35 by opassin           #+#    #+#             */
-/*   Updated: 2022/10/23 13:33:29 by opassin          ###   ########.fr       */
+/*   Updated: 2022/10/25 00:04:58 by opassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,45 @@
 #include <string.h>
 #include <errno.h>
 #include <limits.h>
+#include "../../minishell.h"
+
+
+char	*getenv_val(t_env *envp, char *name)
+{
+	t_var	*var;
+
+	var = envp->var;
+	while (var)
+	{
+		if (!ft_strcmp(var->name, var_name))
+			return (var->value);
+		var = var->next;
+	}
+	return (NULL);
+}
 
 /*
  * ft_cd 		: Change Directory 
  * Type 		: integer
  * Parametre	: aucun
- * Valeur de retour:  errno en cas d'erreur, zero en cas de succes;
+ * Valeur de retour: 0 en cas de succes;
  */
+ 
+ int ft_cd(t_env *envp, char *path)
+{
+	char *cdpath;
+
+	if (!path)
+	{
+		cdpath = getenv_val(envp, "HOME");
+		if (ft_strlen(cdpath) == 0 )
+		{
+			printf("minishell: cd: HOME not set\n");
+			return (1);
+		}
+		else if (path)
+
+	}
+	
+
+}
