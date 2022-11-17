5edbddad57d4774e91a5e9e28848e31573d034a5
Riyettes <ccouliba@student.42.fr>
2022-11-18 00:37:43 +0100

    merge

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ccouliba <ccouliba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 04:04:03 by ccouliba          #+#    #+#             */
-/*   Updated: 2022/11/17 23:37:25 by ccouliba         ###   ########.fr       */
+/*   Updated: 2022/11/18 00:35:45 by ccouliba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,9 +70,8 @@ int	main(int ac, char **av, char **env)
 	(void)ac;
 	(void)av;
 	s = NULL;
-	if (!env)
-		return (0);
-	envp = ft_getenv(env);
+	if (*env)
+		envp = ft_getenv(env);
 	start = NULL;
 	if (signal(SIGINT, sig_handler) == SIG_ERR)
 		return (EXIT_FAILURE);
@@ -82,7 +81,7 @@ int	main(int ac, char **av, char **env)
 	{
 		s = readline((const char *)ft_shellname());
 		if (s == NULL)
-			return (gc_free(), printf("exit\n"), EXIT_FAILURE);
+			return (gc_free(), printf("\nexit\n"), EXIT_FAILURE);
 		if (s && *s && ft_not_only_space((void *)s))
 			g_status = ft_minishell(envp, s, g_status);
 	}
