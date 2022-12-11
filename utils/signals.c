7f65e2516f59eccc7c18c5b15f5cbabfd778bbfa
Riyettes <ccouliba@student.42.fr>
2022-12-11 06:20:42 +0100

    var

--- utils/signals.c
+++ utils/signals.c
@@ -6,12 +6,15 @@
 /*   By: ccouliba <ccouliba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 01:39:55 by ccouliba          #+#    #+#             */
-/*   Updated: 2022/12/11 06:01:31 by ccouliba         ###   ########.fr       */
+/*   Updated: 2022/12/11 06:20:09 by ccouliba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+/*
+** Can't have the right return when interrupt a process by SIGINT
+*/
 void	sig_handler(int sig)
 {
 	if (sig == SIGINT)
@@ -25,3 +28,41 @@ void	sig_handler(int sig)
 	}
 	return ;
 }
+
+// void	parent_signal(int sig)
+// {
+// 	if (sig == SIGQUIT)
+// 		ft_putstr_fd("\b\b  \b\b", 2);
+// 	if (sig == SIGINT)
+// 	{
+// 		ft_putstr_fd("\n", 1);
+// 		rl_replace_line("", 1);
+// 		rl_on_new_line();
+// 		rl_redisplay();
+// 		g_status = 130;
+// 	}
+// }
+
+// void	child_signal(int sig)
+// {
+// 	if (sig == SIGQUIT)
+// 	{
+// 		ft_putstr_fd("Quit: (core dumped)\n", 2);
+// 		g_status = 131;
+// 		// g_ms.sigquit = 1;
+// 	}
+// 	else if (sig == SIGINT)
+// 	{
+// 		ft_putstr_fd("\n", 2);
+// 		g_status = 130;
+// 		// g_ms.sigint = 1;
+// 	}
+// }
+
+// void	get_signal(int sig)
+// {
+// 	// if (g_ms.pid)
+// 		child_signal(sig);
+// 	// else
+// 		parent_signal(sig);
+// }
