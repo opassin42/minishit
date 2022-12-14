4c90c342d1fadf3bd21484b12d9db7117d774563
Riyettes <ccouliba@student.42.fr>
2022-12-14 04:26:39 +0100

    null version

---
+++ lol
@@ -0,0 +1,6 @@
+
+[?2004h[0;32m/mnt/nfs/homes/ccouliba/Documents/minishit:$>[0m[?2004l
+[?2004h[0;32m/mnt/nfs/homes/ccouliba/Documents/minishit:$>[0m[?2004l
+[?2004h[0;32m/mnt/nfs/homes/ccouliba/Documents/minishit:$>[0m^C[?2004l
+
+[0;32m/mnt/nfs/homes/ccouliba/Documents/minishit:$>[0m[?2004h^Z[?2004l
--- srcs/main.c
+++ srcs/main.c
@@ -6,23 +6,32 @@
 /*   By: ccouliba <ccouliba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 04:04:03 by ccouliba          #+#    #+#             */
-/*   Updated: 2022/12/14 02:56:30 by ccouliba         ###   ########.fr       */
+/*   Updated: 2022/12/14 04:26:19 by ccouliba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// t_gc	*start;
-t_data		g_data;
+t_gc	*start;
+int		g_status;
 
 char	*ft_shellname(void)
 {
 	char	*tmp;
 
 	tmp = get_pwd();
+	dup2(STDIN_FILENO, 0);
+	dup2(STDIN_FILENO, 1);
 	tmp = ft_strjoin("\e[0;32m", tmp);
 	tmp = ft_strjoin(tmp, ":$>\e[0m");
 	return (tmp);
+}
+
+void	ft_add_history(void *s)
+{
+	if (s && ft_white_spaces(s))
+		add_history(s);
+	return ;
 }
 
 void	print_token(t_list *token)
@@ -36,72 +45,56 @@ void	print_token(t_list *token)
 	return ;
 }
 
-int	ft_minishell(t_env *envp, char *s, t_data g_data)
+int	ft_minishell(t_env envp, char *s, int g_status)
 {
 	t_list	*token;
 	t_cmd	*cmd;
 
-	if (!s)
-		return (EXIT_SUCCESS);
-	add_history((void *)s);
+	ft_add_history((void *)s);
 	token = (t_list *)ft_lexer(s);
 	if (!token)
-		return (g_data.status);
-	g_data.status = ft_parser(&token);
-	if (g_data.status)
-		return (g_data.status);
+		return (g_status);
+	g_status = ft_parser(&token);
+	if (g_status)
+		return (g_status);
 	ft_expander(&token, envp);
 	hash_quote(&token, -1);
 	cmd = ft_cmd(&token);
 	if (cmd)
 	{
-		g_data.status = ft_exec(envp, cmd);
+		g_status = ft_exec(&envp, cmd);
 		if (STDOUT_FILENO != 1)
 			dup2(cmd->finalfdout, STDOUT_FILENO);
 		if (STDIN_FILENO != 0)
 			dup2(cmd->finalfdin, STDIN_FILENO);
 	}
-	return (g_data.status);
-}
-
-int	ft_readline(t_env *envp, char *s)
-{
-	s = readline((const char *)ft_shellname());
-		return (printf("exit\n"), g_data.status = -42);
-	if (s && *s && *s != '\n')
-		g_data.status = ft_minishell(envp, s, g_data);
-	if (s)
-		free(s);
-	s = NULL;
-	return (g_data.status);
+	return (g_status);
 }
 
 int	main(int ac, char **av, char **env)
 {
 	char	*s;
 	t_env	*envp;
-	t_data	g_data;
 
 	(void)ac;
 	(void)av;
 	s = NULL;
-	g_data = init_global();
-	if (isatty(STDIN_FILENO) == 0)
-		return (gc_free(), 0);
+	start = NULL;
+	// if (!env || !env[0])
+	// 	env[0] = ft_strdup(0);
 	if (signal(SIGINT, sig_handler) == SIG_ERR)
 		return (EXIT_FAILURE);
 	if (signal(SIGQUIT, SIG_IGN))
-		g_data.keeprunning = 1;
-	init_signal();
-	signal(SIGINT, sig_handler);
-	signal(SIGQUIT, sig_handler);
+		keepRunning = 1;
 	envp = ft_getenv(env);
-	while (g_data.keeprunning)
+	while (keeprunning)
 	{
-		init_signal();
-		g_data.status = ft_readline(envp, s);
-		if (g_data.status == -42)
-			return (gc_free(), 0);
+		s = readline((const char *)ft_shellname());
+		if (s == NULL)
+			return (gc_free(), printf("exit\n"), g_status);
+		if (s && *s && ft_not_only_space((void *)s))
+			g_status = ft_minishell(envp, s, g_status);
 	}
-	return (gc_free(), 0);
+	gc_free();
+	return (0);
 }
